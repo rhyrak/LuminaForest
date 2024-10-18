d5a3c5c4d5407685e56ee727821f30e305536784
Yousif <yousifhazardous@gmail.com>
2024-10-19 00:54:58 +0300

    Fix player's jump and sprite orientation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,17 +7,24 @@ using UnityEngine.UIElements;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float walkSpeed = 4f;
+    [SerializeField] private float jumpForce = 5f;
     [SerializeField] private float runSpeed = 8f;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Vector2 boxSize;
+    [SerializeField] private float castDistance;
 
-    public bool IsGrounded { get; private set; } = true;
+
     public bool IsWalking { get; private set; } = false;
     public bool IsRunning { get; private set; } = false;
     private Rigidbody2D _rigidBody2D;
-    private int counter = 0;
+    private Vector3 originalScale;
+    private float horizontalInput;
+    private bool isJumping = false;
 
     void Awake()
     {
         _rigidBody2D = GetComponent<Rigidbody2D>();
+        originalScale = transform.localScale;
     }
 
     void Start()
@@ -27,25 +34,68 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        HandleMovement();
+        HandleJump();
+        FlipSprite();
+    }
 
+    // Handles horizontal movement
+    private void HandleMovement()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+    // Handles jumping logic
+    private void HandleJump()
+    {
+        if (IsGrounded() && Input.GetButton("Jump"))
+        {
+            isJumping = true; // Set the jump flag
+        }
+    }
+
+    // Handles sprite flipping based on movement direction
+    private void FlipSprite()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+
+        if (moveInput < 0) // Moving left
+        {
+            transform.localScale = new Vector3(-Mathf.Abs(originalScale.x), originalScale.y, originalScale.z);
+        }
+        else if (moveInput > 0) // Moving right
+        {
+            transform.localScale = new Vector3(Mathf.Abs(originalScale.x), originalScale.y, originalScale.z);
+        }
+    }
+
+    public bool IsGrounded()
+    {
+        if (Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, castDistance, groundLayer))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position - transform.up * castDistance, boxSize);
     }
 
     void FixedUpdate()
     {
-        _rigidBody2D.velocity = new Vector2(walkSpeed * Input.GetAxis("Horizontal"), _rigidBody2D.velocity.y);
-        if (IsGrounded && Input.GetButton("Jump"))
+        // Update Horizontal Velocity
+        _rigidBody2D.velocity = new Vector2(horizontalInput * walkSpeed, _rigidBody2D.velocity.y);
+
+        // Update Vertical Velocity
+        if (isJumping)
         {
-            _rigidBody2D.velocity += new Vector2(0, runSpeed);
-            IsGrounded = false;
-        }
-        if (!IsGrounded)
-        {
-            counter++;
-            if (counter == 10)
-            {
-                IsGrounded = true;
-                counter = 0;
-            }
+            _rigidBody2D.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            isJumping = false;  // Reset the jump flag
         }
     }
 }
