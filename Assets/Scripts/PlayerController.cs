780c880cb996a4d22195c23c99689db1521e17d3
Yousif <yousifhazardous@gmail.com>
2024-10-19 16:54:08 +0300

    Add character Animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
 
 [RequireComponent(typeof(Rigidbody2D))]
@@ -13,17 +14,79 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Vector2 boxSize;
     [SerializeField] private float castDistance;
 
+    private bool _isMoving = false;
+    private bool _isRunning = false;
+    private bool _isGrounded = false;
+    private bool _isJumping = false;
 
-    public bool IsWalking { get; private set; } = false;
-    public bool IsRunning { get; private set; } = false;
+    public bool IsMoving
+    {
+        get
+        {
+            return _isMoving;
+        }
+        private set
+        {
+            _isMoving = value;
+            _animator.SetBool("isMoving", value);
+        }
+    }
+    public bool IsRunning
+    {
+        get
+        {
+            return _isRunning;
+        }
+        private set
+        {
+            _isRunning = value;
+            _animator.SetBool("isRunning", value);
+        }
+    }
+    public bool IsGrounded
+    {
+        get
+        {
+            return _isGrounded;
+        }
+        private set
+        {
+            _isGrounded = value;
+            _animator.SetBool("isGrounded", value);
+        }
+    }
+    public bool IsJumping
+    {
+        get
+        {
+            return _isJumping;
+        }
+        private set
+        {
+            _isJumping = value;
+            _animator.SetBool("isJumping", value);
+        }
+    }
+    public float CurrentMoveSpeed
+    {
+        get
+        {
+            if (IsMoving)
+                return IsRunning ? runSpeed : walkSpeed;
+            else
+                return 0;
+        }
+    }
+
     private Rigidbody2D _rigidBody2D;
+    private Animator _animator;
     private Vector3 originalScale;
-    private float horizontalInput;
-    private bool isJumping = false;
+    private Vector2 moveInput;
 
     void Awake()
     {
         _rigidBody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
         originalScale = transform.localScale;
     }
 
@@ -34,42 +97,52 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        HandleMovement();
-        HandleJump();
         FlipSprite();
     }
 
-    // Handles horizontal movement
-    private void HandleMovement()
+    // Handles movement
+    public void OnMove(InputAction.CallbackContext context)
     {
-        horizontalInput = Input.GetAxis("Horizontal");
+        moveInput = context.ReadValue<Vector2>();
+
+        IsMoving = moveInput.x != 0;
     }
 
-    // Handles jumping logic
-    private void HandleJump()
+    public void OnRun(InputAction.CallbackContext context)
     {
-        if (IsGrounded() && Input.GetButton("Jump"))
+        if (context.started)
         {
-            isJumping = true; // Set the jump flag
+            IsRunning = true;
+        }
+        else if (context.canceled)
+        {
+            IsRunning = false;
+        }
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.started && CheckIsGrounded())
+        {
+            IsJumping = true; // Set the jump flag
+            IsGrounded = false; // Set the grounded flag to false
         }
     }
 
     // Handles sprite flipping based on movement direction
     private void FlipSprite()
     {
-        float moveInput = Input.GetAxis("Horizontal");
-
-        if (moveInput < 0) // Moving left
+        if (moveInput.x < 0) // Moving left
         {
             transform.localScale = new Vector3(-Mathf.Abs(originalScale.x), originalScale.y, originalScale.z);
         }
-        else if (moveInput > 0) // Moving right
+        else if (moveInput.x > 0) // Moving right
         {
             transform.localScale = new Vector3(Mathf.Abs(originalScale.x), originalScale.y, originalScale.z);
         }
     }
 
-    public bool IsGrounded()
+    public bool CheckIsGrounded()
     {
         if (Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, castDistance, groundLayer))
         {
@@ -89,13 +162,18 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         // Update Horizontal Velocity
-        _rigidBody2D.velocity = new Vector2(horizontalInput * walkSpeed, _rigidBody2D.velocity.y);
+        _rigidBody2D.velocity = new Vector2(moveInput.x * CurrentMoveSpeed, _rigidBody2D.velocity.y);
+
+        if (CheckIsGrounded() && !IsJumping)
+        {
+            IsGrounded = true;
+        }
 
         // Update Vertical Velocity
-        if (isJumping)
+        if (IsJumping)
         {
             _rigidBody2D.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
-            isJumping = false;  // Reset the jump flag
+            IsJumping = false;
         }
     }
 }
